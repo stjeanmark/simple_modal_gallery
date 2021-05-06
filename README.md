c25381fdd951d9a2b6c14728ed87aef5a375992f
Mark St. Jean <52003247+stjeanmark@users.noreply.github.com>
2021-05-06 11:57:30 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple Modal Gallery
+by Mark St. Jean ( http://www.markstjean.com )
+
+A simple gallery where each image can have multiple hotspots which trigger a modal box.
---
+++ tl_simple_modal_gallery.php
@@ -0,0 +1,159 @@
+<?php
+
+/**
+ * Simple Modal Gallery - A simple gallery where each image can have multiple hotspots which trigger a modal box.
+ *
+ * Copyright (C) 2021 Mark St. Jean.
+ *
+ * @package    stjeanmark/simple_modal_gallery
+ * @link       http://www.markstjean.com
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+ 
+/**
+ * Table tl_simple_modal_gallery
+ */
+$GLOBALS['TL_DCA']['tl_simple_modal_gallery'] = array
+(
+ 
+    // Config
+    'config' => array
+    (
+        'dataContainer'               => 'Table',
+        'enableVersioning'            => true,
+        'sql' => array
+        (
+            'keys' => array
+            (
+                'id' 	=> 	'primary',
+                'alias' =>  'index'
+            )
+        )
+    ),
+ 
+    // List
+    'list' => array
+    (
+        'sorting' => array
+        (
+            'mode'                    => 0,
+            'panelLayout'             => 'filter;search,limit'
+        ),
+        'label' => array
+        (
+            'fields'                  => array('product_name', 'product_inventory'),
+            'format'                  => '%s <span style="font-weight: bold;">(%s)</span>'
+        ),
+        'global_operations' => array
+        (
+            'all' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['MSC']['all'],
+                'href'                => 'act=select',
+                'class'               => 'header_edit_all',
+                'attributes'          => 'onclick="Backend.getScrollOffset()" accesskey="e"'
+            )
+        ),
+        'operations' => array
+        (
+            'edit' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['edit'],
+                'href'                => 'act=edit',
+                'icon'                => 'edit.gif'
+            ),
+			
+            'copy' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['copy'],
+                'href'                => 'act=copy',
+                'icon'                => 'copy.gif'
+            ),
+            'delete' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['delete'],
+                'href'                => 'act=delete',
+                'icon'                => 'delete.gif',
+                'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+            ),
+            'toggle' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['toggle'],
+				'icon'                => 'visible.gif',
+				'attributes'          => 'onclick="Backend.getScrollOffset();return AjaxRequest.toggleVisibility(this,%s)"',
+				'button_callback'     => array('Asc\Backend\SimpleModalGalleryBackend', 'toggleIcon')
+			),
+            'show' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['show'],
+                'href'                => 'act=show',
+                'icon'                => 'show.gif'
+            )
+        )
+    ),
+ 
+    // Palettes
+    'palettes' => array
+    (
+        'default'                     => '{product_legend},product_name,product_inventory;{publish_legend},published;'
+    ),
+ 
+    // Fields
+    'fields' => array
+    (
+        'id' => array
+        (
+		'sql'                     	=> "int(10) unsigned NOT NULL auto_increment"
+        ),
+        'tstamp' => array
+        (
+		'sql'                     	=> "int(10) unsigned NOT NULL default '0'"
+        ),
+	'sorting' => array
+	(
+		'sql'                    	=> "int(10) unsigned NOT NULL default '0'"
+	),
+	'alias' => array
+	(
+		'label'                   => &$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['alias'],
+		'exclude'                 => true,
+		'inputType'               => 'text',
+		'search'                  => true,
+		'eval'                    => array('unique'=>true, 'rgxp'=>'alias', 'doNotCopy'=>true, 'maxlength'=>128, 'tl_class'=>'w50'),
+		'save_callback' => array
+		(
+			array('Asc\Backend\SimpleModalGalleryBackend', 'generateAlias')
+		),
+		'sql'                     => "varchar(128) COLLATE utf8_bin NOT NULL default ''"
+
+	),
+	'product_name' => array
+	(
+		'label'                   => &$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['product_name'],
+		'inputType'               => 'text',
+		'default'		  => '',
+		'search'                  => true,
+		'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+		'sql'                     => "varchar(255) NOT NULL default ''"
+	),
+	'product_inventory' => array
+	(
+		'label'                   => &$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['product_inventory'],
+		'inputType'               => 'text',
+		'default'		  => '',
+		'search'                  => true,
+		'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
+		'sql'                     => "int(10) unsigned NOT NULL default '0'"
+	),
+	'published' => array
+	(
+		'exclude'                 => true,
+		'label'                   => &$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['published'],
+		'inputType'               => 'checkbox',
+		'eval'                    => array('submitOnChange'=>true, 'doNotCopy'=>true),
+		'sql'                     => "char(1) NOT NULL default ''"
+	)		
+    )
+);
+
