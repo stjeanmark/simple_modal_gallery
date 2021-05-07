eba09aae9207926fad02f269f46fe96378b7ca5f
Mark St. Jean <52003247+stjeanmark@users.noreply.github.com>
2021-05-07 10:08:37 -0400

    Update tl_simple_modal_gallery.php

--- system/modules/simple_modal_gallery/languages/en/tl_simple_modal_gallery.php
+++ system/modules/simple_modal_gallery/languages/en/tl_simple_modal_gallery.php
@@ -14,9 +14,9 @@
 /**
  * Legends
  */
-$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['slide_legend']   	= 'Image Details';
+$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['slide_legend']     	= 'Image Details';
 $GLOBALS['TL_LANG']['tl_simple_modal_gallery']['hotspot_legend']   	= 'Hotspot Details';
-$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['publish_legend'] 	= 'Publish';
+$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['publish_legend']   	= 'Publish';
 
 /**
  * Fields
@@ -25,22 +25,22 @@ $GLOBALS['TL_LANG']['tl_simple_modal_gallery']['alias'] 			           = array('A
 $GLOBALS['TL_LANG']['tl_simple_modal_gallery']['product_name'] 			   	= array('Product Name', 'The name of the product');
 $GLOBALS['TL_LANG']['tl_simple_modal_gallery']['product_inventory'] 		= array('Product Inventory', 'The inventory total');
 
-$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['slide_image'] 		= array('Select the Slide\'s Image', 'Select the image we will use for this particular slide');
-$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['slide_name'] 		= array('Slide Name', 'Give this slide a name');
+$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['slide_image'] 	      	= array('Select the Slide\'s Image', 'Select the image we will use for this particular slide');
+$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['slide_name'] 	       	= array('Slide Name', 'Give this slide a name');
 
-$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['hotspot_links'] 		= array('Hotspot Links', 'The hotspot links that will be on the slide');
-$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['hotspot_x'] 		= array('Hotspot X', 'X coordinate for the hotspot link on the image');
-$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['hotspot_y'] 		= array('Hotspot Y', 'Y coordinate for the hotspot link on the image');
-$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['hotspot_text'] 		= array('Hotspot Text', 'The text that will show in the modal box');
+$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['hotspot_links']     		= array('Hotspot Links', 'The hotspot links that will be on the slide');
+$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['hotspot_x']         		= array('Hotspot X', 'X coordinate for the hotspot link on the image');
+$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['hotspot_y'] 	        	= array('Hotspot Y', 'Y coordinate for the hotspot link on the image');
+$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['hotspot_text']      		= array('Hotspot Text', 'The text that will show in the modal box');
 
 $GLOBALS['TL_LANG']['tl_simple_modal_gallery']['published'] 		        = array('Published', 'Save this product\'s record.');
  
  /**
  * Buttons
  */
-$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['new']   		 	= array('New record', 'Add a new record');
-$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['show']  		 	= array('Record details', 'Show the details of record ID %s');
-$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['edit']  		 	= array('Edit record', 'Edit record ID %s');
-$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['copy']  		 	= array('Copy record', 'Copy record ID %s');
-$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['delete'] 			= array('Delete record', 'Delete record ID %s');
-$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['toggle'] 			= array('Toggle record', 'Toggle record ID %s');
+$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['new']   		 	= array('New Slide', 'Add a new slide');
+$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['show']  		 	= array('Slide Details', 'Show the details of slide ID %s');
+$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['edit']  		 	= array('Edit Slide', 'Edit slide ID %s');
+$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['copy']  		 	= array('Copy Slide', 'Copy slide ID %s');
+$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['delete'] 			= array('Delete Slide', 'Delete slide ID %s');
+$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['toggle'] 			= array('Toggle Slide Published', 'Toggle slide ID %s');
