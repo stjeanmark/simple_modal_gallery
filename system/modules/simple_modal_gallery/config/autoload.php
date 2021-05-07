d8391af0f15bb13e2890b7f1991bf15a5739d021
Mark St. Jean <52003247+stjeanmark@users.noreply.github.com>
2021-05-07 12:21:36 -0400

    Update autoload.php

--- system/modules/simple_modal_gallery/config/autoload.php
+++ system/modules/simple_modal_gallery/config/autoload.php
@@ -10,16 +10,21 @@
  * @license    http://opensource.org/licenses/lgpl-3.0.html
  */
 
-
-/**
- * Register the classes
- */
+/* Register the classes */
 ClassLoader::addClasses(array
 (
+	'Asc\Module\GalleryModule' 		=> 'system/modules/simple_modal_gallery/library/Asc/Module/GalleryModule.php',
 	'Asc\Model\SimpleModalGallery' 		=> 'system/modules/simple_modal_gallery/library/Asc/Model/SimpleModalGallery.php'
 ));
 
 ClassLoader::addClasses(array
 (
 	'CustomTags\SimpleModalGalleryInsertTag' 		=> 'system/modules/simple_modal_gallery/library/CustomTags/SimpleModalGalleryInsertTag.php'
+));
+
+/* Register the templates */
+TemplateLoader::addFiles(array
+(
+    	'mod_gallery_module' 		=> 'system/modules/simple_modal_gallery/templates/modules',
+	'item_slide' 			=> 'system/modules/simple_modal_gallery/templates/items',
 ));
