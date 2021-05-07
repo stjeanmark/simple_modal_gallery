b3934d2fd04a7e2af723c96d11c7290259832e95
Mark St. Jean <52003247+stjeanmark@users.noreply.github.com>
2021-05-07 12:23:12 -0400

    Update config.php

--- system/modules/simple_modal_gallery/config/config.php
+++ system/modules/simple_modal_gallery/config/config.php
@@ -11,26 +11,21 @@
  */
 
 
-/**
-* Back end modules
-*/
-
-// DECLARING LOCATIONS BACK END PLUGIN
+/* Back end modules */
 $GLOBALS['BE_MOD']['content']['simple_modal_gallery'] = array(
 	'tables' => array('tl_simple_modal_gallery'),
 	'icon'   => 'system/modules/simple_modal_gallery/assets/icons/simple_modal_gallery.png',
 	'exportLocations' => array('Asc\Backend\SimpleModalGalleryBackend', 'exportSimpleModalGallery')
 );
 
-/**
- * Models
- */
+/* Front end modules */
+$GLOBALS['FE_MOD']['simple_modal_gallery']['gallery_module'] 	= 'Asc\Module\GalleryModule';
+
+/* Models */
 $GLOBALS['TL_MODELS']['tl_simple_modal_gallery'] = 'Asc\Model\SimpleModalGallery';
 
 
-/**
- * Hooks
- */
+/* Hooks */
 if (\Config::getInstance()->isComplete()) {
 	$GLOBALS['TL_HOOKS']['replaceInsertTags'][] = array('CustomTags\SimpleModalGalleryInsertTag', 'onReplaceTag');
 }
