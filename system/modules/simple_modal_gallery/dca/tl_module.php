befd09e3c1a081383fa058b8c60de6b10e5b77e4
Mark St. Jean <52003247+stjeanmark@users.noreply.github.com>
2021-05-07 12:26:23 -0400

    Update tl_module.php

--- system/modules/simple_modal_gallery/dca/tl_module.php
+++ system/modules/simple_modal_gallery/dca/tl_module.php
@@ -1,27 +1,14 @@
 <?php
 
 /**
- * Locations - Location Plugin for Contao
+ * Simple Modal Gallery - A simple gallery where each image can have multiple hotspots which trigger a modal box.
  *
- * Copyright (C) 2018 Andrew Stevens
+ * Copyright (C) 2021 Mark St. Jean.
  *
- * @package    asconsulting/locations
- * @link       http://andrewstevens.consulting
+ * @package    stjeanmark/simple_modal_gallery
+ * @link       http://www.markstjean.com
  * @license    http://opensource.org/licenses/lgpl-3.0.html
  */
 
-
-/**
- * Add a palette to tl_module
- */
-$GLOBALS['TL_DCA']['tl_module']['palettes']['locations_list'] 		= '{title_legend},name,headline,type;{template_legend:hide},customTpl,locations_customItemTpl;{expert_legend:hide},guests,cssID,space';
-
-$GLOBALS['TL_DCA']['tl_module']['fields']['locations_customItemTpl'] = array
-(
-	'label'                   => &$GLOBALS['TL_LANG']['tl_module']['customItemTpl'],
-	'exclude'                 => true,
-	'inputType'               => 'select',
-	'options_callback'        => array('Asc\Backend\Locations', 'getItemTemplates'),
-	'eval'                    => array('includeBlankOption'=>true, 'chosen'=>true, 'tl_class'=>'w50'),
-	'sql'                     => "varchar(64) NOT NULL default ''"
-);
+/* Add a palette to tl_module */
+$GLOBALS['TL_DCA']['tl_module']['palettes']['gallery_module'] 		= '{title_legend},name,headline,type;{template_legend:hide},customTpl;{expert_legend:hide},guests,cssID,space';
