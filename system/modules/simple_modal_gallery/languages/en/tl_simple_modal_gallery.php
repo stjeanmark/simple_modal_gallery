2c3ed4209aadb761bfa992b7ac99efa1b7e7c90a
Mark St. Jean <52003247+stjeanmark@users.noreply.github.com>
2021-05-13 11:17:30 -0400

    Update tl_simple_modal_gallery.php

--- system/modules/simple_modal_gallery/languages/en/tl_simple_modal_gallery.php
+++ system/modules/simple_modal_gallery/languages/en/tl_simple_modal_gallery.php
@@ -23,6 +23,7 @@ $GLOBALS['TL_LANG']['tl_simple_modal_gallery']['product_inventory'] 		= array('P
 
 $GLOBALS['TL_LANG']['tl_simple_modal_gallery']['slide_image'] 	      	= array('Select the Slide\'s Image', 'Select the image we will use for this particular slide');
 $GLOBALS['TL_LANG']['tl_simple_modal_gallery']['slide_name'] 	       	= array('Slide Name', 'Give this slide a name');
+$GLOBALS['TL_LANG']['tl_simple_modal_gallery']['hotspot_icon'] 	      = array('Hotspot Icon', 'Insert code for the hotspot links\'s icon');
 
 $GLOBALS['TL_LANG']['tl_simple_modal_gallery']['hotspot_links']     		= array('Hotspot Links', 'The hotspot links that will be on the slide');
 $GLOBALS['TL_LANG']['tl_simple_modal_gallery']['hotspot_x']         		= array('Hotspot X', 'X coordinate for the hotspot link on the image');
