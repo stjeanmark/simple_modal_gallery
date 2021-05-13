81f773c543282dd5413ff901945ac3c48c646137
Mark St. Jean <52003247+stjeanmark@users.noreply.github.com>
2021-05-13 13:15:52 -0400

    Update Wizard.php

--- system/modules/simple_modal_gallery/library/Asc/Backend/Wizard.php
+++ system/modules/simple_modal_gallery/library/Asc/Backend/Wizard.php
@@ -20,19 +20,11 @@ class Wizard
     public function onProductTypeWizard(DataContainer $dc)
     {
         if ($dc->value < 1) {
-            return '';
+            return ' nothing ';
         }
 
         return sprintf(
-            ' asdf<a href="%s" title="%s" style="padding-left:3px" onclick="Backend.openModalIframe({\'width\':300,\'title\':\'%s\',\'url\':this.href});return false">%s</a>',
-            sprintf(
-                'contao/main.php?do=iso_setup&amp;mod=producttypes&amp;table=tl_iso_producttype&amp;act=edit&amp;id=%s&amp;popup=1&amp;nb=1&amp;rt=%s',
-                $dc->value,
-                REQUEST_TOKEN
-            ),
-            sprintf(specialchars($GLOBALS['TL_LANG']['tl_iso_producttype']['edit'][1]), $dc->value),
-            specialchars(str_replace("'", "\\'", sprintf($GLOBALS['TL_LANG']['tl_iso_producttype']['edit'][1], $dc->value))),
-            Image::getHtml('alias.gif', $GLOBALS['TL_LANG']['tl_iso_producttype']['edit'][0])
+            ' something ';
         );
 
     }
