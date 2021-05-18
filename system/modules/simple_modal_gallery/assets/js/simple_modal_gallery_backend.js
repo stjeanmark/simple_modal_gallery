2a3b7600cd852a36674335e58d28bda0377aee93
Mark St. Jean <52003247+stjeanmark@users.noreply.github.com>
2021-05-18 13:09:10 -0400

    Update simple_modal_gallery_backend.js

--- system/modules/simple_modal_gallery/assets/js/simple_modal_gallery_backend.js
+++ system/modules/simple_modal_gallery/assets/js/simple_modal_gallery_backend.js
@@ -9,18 +9,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 var $input = document.getElementById("ctrl_slide_image");
-var last$inputValue = $input.value;
-setInterval(function() {
-    var newValue = $input.value;
-    if (last$inputValue != newValue) {
-        last$inputValue = newValue;
-        handleValueChange();
-    }
-}, 50); // 20 times/second
-function handleValueChange() {
-    console.log("$input's value changed: " + $input.value);
-}
-// Trigger a change
-setTimeout(function() {
-    $input.value = "new value";
-}, 800);
+
+var event = new Event('input');
+$input.addEventListener('input', function () { 
+  console.log('ding');
+});
+
+$input.value = 'something';
+$input.dispatchEvent(input);
