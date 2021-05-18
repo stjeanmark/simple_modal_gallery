7fe9b39287142a09282d2592c66f6f227b145ea4
Mark St. Jean <52003247+stjeanmark@users.noreply.github.com>
2021-05-18 13:33:23 -0400

    Update simple_modal_gallery_backend.js

--- system/modules/simple_modal_gallery/assets/js/simple_modal_gallery_backend.js
+++ system/modules/simple_modal_gallery/assets/js/simple_modal_gallery_backend.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("ctrl_hotspot_links").innerHTML += 
-              "<h3 id='update_helper'>Helper Image Goes Here</h3>";
+    //document.getElementById("ctrl_hotspot_links").innerHTML += 
+             // "<h3 id='update_helper'>Helper Image Goes Here</h3>";
 });
 
 
@@ -8,14 +8,17 @@ document.addEventListener("DOMContentLoaded", function() {
 var intervalId = window.setInterval(function(){
     
     var picture = document.getElementsByClassName("gimage");
+    var toAppend = document.getElementById("pal_slide_legend");
     var src = picture[0].src;
     
     if(src != '') {
         console.log("image Source: " + src);
+        
+        toAppend.append("<img src='" + src + "' width='100%'>");
+        
+        //stop running once we have found our image
+        clearInterval(intervalId) 
     }
-    
-    console.log("Anyway: " + src);
-    
     
 }, 5000);
 
