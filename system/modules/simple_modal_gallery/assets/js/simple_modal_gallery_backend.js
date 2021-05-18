6ee25e2ae07a7876669c394196577746491ac87b
Mark St. Jean <52003247+stjeanmark@users.noreply.github.com>
2021-05-18 14:29:09 -0400

    Update simple_modal_gallery_backend.js

--- system/modules/simple_modal_gallery/assets/js/simple_modal_gallery_backend.js
+++ system/modules/simple_modal_gallery/assets/js/simple_modal_gallery_backend.js
@@ -14,10 +14,56 @@ var intervalId = window.setInterval(function(){
     
     if(src != '') {
 
-       toAppend.insertAdjacentHTML("afterend", "<img style='margin-top:5px; padding:10px;' src='" + src + "' width='100%'>");
+       toAppend.insertAdjacentHTML("afterend", "<img id='hotspot_image' style='margin-top:5px; padding:10px;' src='" + src + "' width='100%'>");
+        
+        var myImg = document.getElementById("hotspot_image");
+        myImg.onmousedown = GetCoordinates;
         
         //stop running once we have found our image
         clearInterval(intervalId) 
     }
 }, 1000);
 
+
+
+function FindPosition(oElement)
+{
+  if(typeof( oElement.offsetParent ) != "undefined")
+  {
+    for(var posX = 0, posY = 0; oElement; oElement = oElement.offsetParent)
+    {
+      posX += oElement.offsetLeft;
+      posY += oElement.offsetTop;
+    }
+      return [ posX, posY ];
+    }
+    else
+    {
+      return [ oElement.x, oElement.y ];
+    }
+}
+
+function GetCoordinates(e)
+{
+  var PosX = 0;
+  var PosY = 0;
+  var ImgPos;
+  ImgPos = FindPosition(myImg);
+  if (!e) var e = window.event;
+  if (e.pageX || e.pageY)
+  {
+    PosX = e.pageX;
+    PosY = e.pageY;
+  }
+  else if (e.clientX || e.clientY)
+    {
+      PosX = e.clientX + document.body.scrollLeft
+        + document.documentElement.scrollLeft;
+      PosY = e.clientY + document.body.scrollTop
+        + document.documentElement.scrollTop;
+    }
+  PosX = PosX - ImgPos[0];
+  PosY = PosY - ImgPos[1];
+  document.getElementById("x").innerHTML = PosX;
+  document.getElementById("y").innerHTML = PosY;
+}
